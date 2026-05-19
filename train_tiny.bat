f86b62b311018edb9d4cc0e39158a22d0d8e1563
marklalon <marklalon@gmail.com>
2026-05-19 23:36:18 +0800

    bump: update run name to v2 and increase layers to 8

--- train_tiny.bat
+++ train_tiny.bat
@@ -1,7 +1,7 @@
 @echo off
 set SCRIPT_DIR=%~dp0
 set PYTHON_EXE=%SCRIPT_DIR%..\.venv\Scripts\python.exe
-set RUN_NAME=quadropeds_locomotion_cross_limb_mask_v1
+set RUN_NAME=quadropeds_locomotion_cross_limb_mask_v2
 
 pushd "%SCRIPT_DIR%"
 
@@ -17,7 +17,7 @@ REM 支持任何在数据集中的物种名称，如 Dragon, Bird, Camel 等
 	--train_split train ^
 	--action_tags locomotion ^
 	--latent_dim 256 ^
-	--layers 6 ^
+	--layers 8 ^
 	--cross_limb_last_n 3 ^
 	--temporal_window 41 ^
 	--diffusion_steps 100 ^
