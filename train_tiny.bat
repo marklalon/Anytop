fecd3e2bcf11e2b9e734f591f81e4f8bf577f613
marklalon <marklalon@gmail.com>
2026-05-29 23:05:45 +0800

    chore: update run name to quadropeds_locomotion_slim_v1

--- train_tiny.bat
+++ train_tiny.bat
@@ -1,7 +1,7 @@
 @echo off
 set SCRIPT_DIR=%~dp0
 set PYTHON_EXE=%SCRIPT_DIR%..\.venv\Scripts\python.exe
-set RUN_NAME=quadropeds_locomotion_varying_length_v7
+set RUN_NAME=quadropeds_locomotion_slim_v1
 
 pushd "%SCRIPT_DIR%"
 
